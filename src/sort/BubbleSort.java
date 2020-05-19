3c0732cb11284a423bc2199d72b69f48a11b4a57
Wangsg <Wangsg@qq.com>
2020-05-19 09:52:32 +0800

    每日一练 bubble算法改进版

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -8,32 +8,32 @@ import java.util.Arrays;
  */
 public class BubbleSort {
     public static void main(String[] args) {
-        int [] arrayList ={42,23,51,33,534,2,65,3,466,44};
+        int [] arrayList ={42,51,33,534,23,2,65,3,466,44};
        // int []arrayList= {1,2,3,4,5,6,6,7};
-        int[] ints = bubbleSort(arrayList);
-        System.out.println(Arrays.toString(ints));
+         bubbleSort(arrayList);
+        System.out.println(Arrays.toString( arrayList));
     }
-    public  static  int[] bubbleSort(int [] array){
-        if(array == null && array.length<2){
-            return array;
+    public  static  void  bubbleSort(int [] array) {
+        if(array == null && array.length < 2){
+            return;
         }
-        for(int i = 0 ;i < array.length ; i ++){
-            boolean flag = false;
-            for (int j = i + 1 ;j <array.length ;j ++){
-                if(array[i] > array[j]){
+        boolean flag = false;
+        for(int i = 0 ;i <array.length - 1 ;i ++){
+            for(int j = 0 ; j < array.length - 1 - i; j ++){
+                if(array[j] > array[j + 1]){
                     flag = true;
-                    int temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
             }
             if(!flag){
                 System.out.println(i);
                 break;
             }else {
-                flag = false;
+                flag = false ;
             }
+
         }
-        return array;
     }
 }
--- src/sort/InsertSort.java
+++ src/sort/InsertSort.java
@@ -9,26 +9,24 @@ import java.util.Arrays;
 public class InsertSort {
     public static void main(String[] args) {
         int [] arrayList ={42,23,51,33,534,2,65,3,466,44};
-        int[] ints = insertSort(arrayList);
-        System.out.println(Arrays.toString(ints));
+        insertSort(arrayList);
+        System.out.println(Arrays.toString(arrayList));
     }
-    public  static  int[] insertSort(int[] arr){
-        if(arr.equals(null)|| arr.length < 2){
-            return arr;
+    public  static  void insertSort(int[] arr) {
+        if(arr ==null && arr.length < 2){
+            return;
         }
-        for(int i = 1 ;i < arr.length; i++){
-            int temp = arr [i];
-            int k = i -1 ;
-            while ( arr[k] >temp && k >=0){
+        for(int i = 1; i < arr.length ;i ++){
+            int temp = arr[i];
+            int k = i - 1;
+            while (k >= 0 && arr[k] >temp){
                 k --;
             }
-            for(int j = i ;j >k + 1 ;j --){
-                arr[j] = arr[j -1];
+            for(int j = i ; j > k+1 ; j --){
+                arr[j] = arr[j - 1];
             }
             arr[k + 1] = temp;
         }
 
-        return arr;
     }
-
 }
