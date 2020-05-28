28f5d08e83bfae1bdfc9c2fbe7d9698682fa9bdc
Wangsg <Wangsg@qq.com>
2020-05-28 10:09:27 +0800

    冒泡排序

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -14,26 +14,25 @@ public class BubbleSort {
         System.out.println(Arrays.toString( arrayList));
     }
     public  static  void  bubbleSort(int [] array) {
-        if(array == null && array.length < 2){
-            return;
+        if(null == array && array.length < 2){
+            return ;
         }
-        boolean flag = false;
-        for(int i = 0 ;i <array.length - 1 ;i ++){
-            for(int j = 0 ; j < array.length - 1 - i; j ++){
+        boolean flag = true;
+        for(int i = 0 ; i < array.length - 1 ; i ++){
+            for( int j = 0 ; j <array.length -1 ; j ++){
                 if(array[j] > array[j + 1]){
-                    flag = true;
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
+                    flag = false;
+                    int temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
                 }
             }
-            if(!flag){
-                System.out.println(i);
+            if(flag){
                 break;
             }else {
-                flag = false ;
+                flag = true;
             }
-
         }
+
     }
 }
